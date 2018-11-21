3cf3c397e627a59b584cf197e2eeb9186a51bf10
Dominik Bydžovský <dominikbydzovsky94@gmail.com>
2018-11-21 20:24:21 +0100

    added deployable

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+
+echo "deployiiiing meee"
--- modules/deploy/deployModule.py
+++ modules/deploy/deployModule.py
@@ -1,7 +1,8 @@
 import argparse
-
+import subprocess
 from modules import module
 import yaml
+import os
 
 class DeployModule(module.Module):
     def command(self):
@@ -11,14 +12,36 @@ class DeployModule(module.Module):
         return "Deploys applications."
 
     def setParser(self, parser: argparse.ArgumentParser):
-        parser.add_argument('-f', type=argparse.FileType('r'), nargs="?", default="Gluefile.yaml", help='A gluefile.yaml to be deployed.')
+        parser.add_argument('gluefile', type=argparse.FileType('r'), nargs="?", default="Gluefile.yaml", help='A gluefile.yaml to be deployed.')
+        parser.add_argument('-deployables', type=argparse.FileType('r'), nargs="*", required=True)
 
     def start(self, args):
-        data = loadData(args)
-        checkStructure(data)
-        print(data)
+        glue = loadData(args)
+        checkStructure(glue)
+        print(glue)
+
+        roles = loadDeployables(args.deployables)
+        checkRoles(roles)
+        print(roles)
+
+        for machine in glue["machines"]:
+            deployMachineRoles(machine, roles)
 
 
+def deployMachineRoles(machine, roles):
+    for want in machine["roles"]:
+        role = next(obj for obj in roles if obj["name"]==want)
+        deployMachineRole(machine, role)
+
+def deployMachineRole(machine, role):
+    # subprocess.call(['C:\\Temp\\a b c\\Notepad.exe', 'C:\\test.txt'])
+    # cwd =
+    # print(cwd)
+    subprocess.call(["/bin/bash","-c",os.getcwd() +"/" +role["deployable"]["bash"]["path"]])
+
+def loadData(args):
+    gluefile = args.gluefile
+    return parseYml(gluefile)
 
 def checkStructure(data):
     print("checking structure.")
@@ -26,15 +49,29 @@ def checkStructure(data):
 
 
 
-def loadData(args):
-    gluefile = args.f
 
+
+def loadDeployables(deployables):
+    res = []
+    for deployable in deployables:
+        res.append(parseYml(deployable))
+    return res
+
+def checkRoles(roles):
+    pass
+
+
+
+
+
+
+def parseYml(openedFile):
     print("loading data.")
     try:
-        y = yaml.load(gluefile)
+        y = yaml.load(openedFile)
         return y
     except yaml.YAMLError as exc:
         print(exc)
         exit(1)
     finally:
-        gluefile.close()
+        openedFile.close()
